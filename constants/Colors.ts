985a813f170fe462bfceadfaaf2f7255760bb79d
Tim <mideltim@gmail.com>
2025-03-11 23:22:07 +0800

    style: fix tab layout and change background color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,21 +1,25 @@
-const tintColorLight = "#2f95dc";
+const tintColorLight = "#0C311E";
 const tintColorDark = "#fff";
+const primaryColor = "#0C311E";
+const secondaryColor = "#8D5D3D";
 
 export default {
   light: {
     text: "#000",
-    background: "#fff",
+    background: "#F9EFE5",
     tint: tintColorLight,
     tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
+    primaryColor: primaryColor,
+    secondaryColor: secondaryColor,
   },
   dark: {
     text: "#fff",
     background: "#000",
-    tint: tintColorDark,
+    tint: primaryColor,
     tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
-    primaryColor: "#279C64",
-    secondaryColor: "#FE9A00",
+    primaryColor: primaryColor,
+    secondaryColor: secondaryColor,
   },
 };
